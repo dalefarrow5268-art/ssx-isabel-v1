27d12e880c616f9151f0120cf7841f6608d2240c
dalefarrow5268-art <dalefarrow5268@gmail.com>
2026-08-06 13:16:49 -0500

    Mount Isabel gaze smoothing runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import IsabelAutonomyLock from "./autonomy-lock";
 import IsabelCommandChoreography from "./command-choreography";
 import IsabelCommandConsole from "./command-console";
+import IsabelGazeRuntime from "./gaze-runtime";
 import IsabelSpeechRuntime from "./speech-runtime";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <IsabelAutonomyLock />
       <IsabelCommandChoreography />
+      <IsabelGazeRuntime />
       <IsabelCommandConsole />
       <IsabelSpeechRuntime />
     </>

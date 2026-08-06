ba2383c09c4e1b320079847e671fce9fa6849e91
dalefarrow5268-art <dalefarrow5268@gmail.com>
2026-08-06 13:19:06 -0500

    Mount Isabel Three.js gaze bridge

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import IsabelAutonomyLock from "./autonomy-lock";
 import IsabelCommandChoreography from "./command-choreography";
 import IsabelCommandConsole from "./command-console";
 import IsabelGazeRuntime from "./gaze-runtime";
+import IsabelGazeThreeBridge from "./gaze-three-bridge";
 import IsabelSpeechRuntime from "./speech-runtime";
 
 export default function Home() {
   return (
     <>
+      <IsabelGazeThreeBridge />
+      <IsabelGazeRuntime />
       <IsabelAutonomyLock />
       <IsabelCommandChoreography />
-      <IsabelGazeRuntime />
       <IsabelCommandConsole />
       <IsabelSpeechRuntime />
     </>

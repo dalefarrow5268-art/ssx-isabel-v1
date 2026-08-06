b779d166ad80728f07f1fc9863d642828af67967
dalefarrow5268-art <dalefarrow5268@gmail.com>
2026-08-06 13:24:24 -0500

    Mount Isabel 20-point placeholder anatomy pass

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import IsabelCommandChoreography from "./command-choreography";
 import IsabelCommandConsole from "./command-console";
 import IsabelGazeRuntime from "./gaze-runtime";
 import IsabelGazeThreeBridge from "./gaze-three-bridge";
+import IsabelPlaceholderAnatomyBridge from "./placeholder-anatomy-bridge";
 import IsabelSpeechRuntime from "./speech-runtime";
 
 export default function Home() {
   return (
     <>
+      <IsabelPlaceholderAnatomyBridge />
       <IsabelGazeThreeBridge />
       <IsabelGazeRuntime />
       <IsabelAutonomyLock />
